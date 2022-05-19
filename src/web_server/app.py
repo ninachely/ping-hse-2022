f3407131938a597fb43b06e12c67a8db3532ceb3
Georgii Shukalov <gshukalov@MacBook-Air-Georgii.local>
2022-05-19 22:48:58 +0400

    Reorganize directory structure

--- src/web_server/app.py
+++ src/web_server/app.py
@@ -1,8 +1,7 @@
-from io import BytesIO, StringIO
-import statistics
+from io import BytesIO
 from threading import Thread
 from typing import Dict
-
+import atexit
 from matplotlib import pyplot as plt
 import matplotlib
 from data import Update, UpdateType
@@ -30,7 +29,6 @@ class Module(DataProcessor):
 
         @self.app.route('/data', methods=['GET'])
         def get_data():
-            try:
                 fig, ax = plt.subplots(2, figsize=(20, 20))
                 df = construct_df(self.datapoints)
                 heatmap(df, ax[0])
@@ -38,9 +36,6 @@ class Module(DataProcessor):
                 return serve_pil_image(
                     fig2img(fig)
                 )
-            except Exception as e:
-                print(e)
-                return "Not enough data yet, wait a bit!"
 
         self.process = Thread(target=self.app.run, args=('localhost', 8080))
         self.process.daemon = True
